9445004ce3ffef354334d9d4139f7ad6b757964f
Kindhee <rmichel@gmaing.tech>
2022-11-29 19:38:49 +0100

    ttttttttt

--- actions/showuser.php
+++ actions/showuser.php
@@ -1,8 +1,11 @@
-<?php 
-require_once "../cfg/config.php"; 
+<?php
+require_once "../cfg/config.php";
 $sql = "SELECT * FROM user"; 
 $pre = $pdo->prepare($sql); 
 $pre->execute();
 $data = $pre->fetchAll(PDO::FETCH_ASSOC);
-foreach($data as $user){ 
-?>
+foreach($data as $user){ ?>
+<div class="bloc_user">
+    <h2><?php echo $user['email']." ".$user['username']." ".$user['admin'] ?></h2>
+</div>
+<?php } ?>
