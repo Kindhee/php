2d54d35abb6a67315fd675ca856139c99a790de9
Kindhee <rmichel@gaming.tech>
2022-11-30 13:54:20 +0100

    aaa

--- actions/admin.php
+++ actions/admin.php
@@ -2,8 +2,8 @@
 require_once "../cfg/config.php"; 
 $sql = "UPDATE user SET admin = :admin WHERE id= :id";
 $dataBinded=array(
-    ':id'   => $_POST['id'],
     ':admin'   => $_POST['admin'],
+    ':id'   => $_POST['id'],
 );
 $pre = $pdo->prepare($sql); 
 $pre->execute($dataBinded);
--- adminpanel.php
+++ adminpanel.php
@@ -34,11 +34,13 @@
   foreach($data as $user){ ?>
   <div class="bloc_user">
     <p class="black-text-css list-text"><?php echo " email : " . $user['email']." | username : ".$user['username']." ".($user['admin']==0?"":" | ADMIN")?></p>
+
     <form method="post" action="actions/admin.php">
-        <input type='hidden' name="id" value = <?php $user['id'] ?> />
-        <?php $isAdmin = $user['admin']==1?0:1  ?> 
-        <input type='hidden' name="admin" value = $isAdmin />
-        <button type="submit">Admin On / Off</button>
+        <input type='hidden' name="id" value = "<?php $user['id'] ?>" />
+        <?php echo $user['id'] ?>
+        <input type='hidden' name="admin" value = "<?php $user['admin']==1?0:1 ?>" />
+        <?php echo $user['admin'] ?>
+        <button type="submit">switch Admin</button>
     </form>
   </div>
   <?php } ?>
