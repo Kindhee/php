fba1b938fe4f0309a3df88f711937124770134c5
Kindhee <rmichel@gmaing.tech>
2022-12-01 09:50:52 +0100

    deee

--- actions/mail.php
+++ actions/mail.php
@@ -3,8 +3,12 @@ $email = "rmichel@gaming.tech";
 $objet = $_POST['object'];
 $content = $_POST['message']."<br><br>Email from : ". $_POST['email'];
 
-if(mail($email,$objet,$content)){
-    echo "mail envoyé";
-}else{
+
+if(empty($email) || empty($email) || empty($email)){
     echo "erreur";
+} else{
+    $_SESSION['success']="Mail envoyé !";
+    // header('Location:../index.php');
+    header('Location:../indexbis.php');
+    exit();
 } ?>
